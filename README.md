1eeab5b0b617385ca975a4302a50b2b87ecf2b08
Phillip Seryazi <seryaziphillip@gmail.com>
2023-07-12 10:13:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## TOPHUT
+A real estate app where property owners can list there properties, monitor and collect rent and tenants can find there ideal homes.
---
+++ src/main/java/com/mudhut/tophut/TophutApplication.java
@@ -0,0 +1,13 @@
+package com.mudhut.tophut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TophutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TophutApplication.class, args);
+	}
+
+}
